c418f1160f877ca416e02275741224d0f3e1bc3d
CarThrottlefan <alexandervici@gmail.com>
2023-09-16 23:10:48 +0200

    Code cleanup

--- src/snake/logic/GameLogic.scala
+++ src/snake/logic/GameLogic.scala
@@ -11,7 +11,6 @@ import snake.logic.GameLogic._
 
 //For reverse: When you play normally, save curr game save on stack then when press reverse just pop from stack
 
-
 class GameLogic(val random: RandomGenerator,
                 val gridDims : Dimensions) {
   case class DirectionBools(var up: Boolean = false, var down: Boolean = false, var left: Boolean = false, var right: Boolean = false)
@@ -39,25 +38,17 @@ class GameLogic(val random: RandomGenerator,
     (x,y)
   }
 
-
   //make a var that is the first index to start the list, and whenever a new location is added, just add a new element to the list. Then, when reverse you just move that index back as many steps needed
 
   var snakeHeadPos: Point = Point(2, 0)
   var snakeHeadDir: Direction = East()
-
-
-  //var currApple : Point = Point (3,3)
-
   var snakeBodyPos: List[Point] = List[Point](Point(1,0), Point(0,0))
-  //val snakeBodyPos : ListBuffer[Point] = ListBuffer (snakeHeadPos, Point(1,0), Point(0,0))
 
   def gameOver: Boolean = {
     tailHit
   }
 
   def appleGenerator() : Point = {
-    //val freeSpotsNum : Int = gridDims.width * gridDims.height - snakeBodyPos.lengt
-
     var free_list = gridDims.allPointsInside.filterNot(point=> snakeBodyPos.contains(point) || point == snakeHeadPos)
 
     if (free_list.isEmpty){
@@ -69,23 +60,23 @@ class GameLogic(val random: RandomGenerator,
     applePlace
   }
 
-  // TODO implement me
-
   var tailHit : Boolean = false
 
   var counter : Int = 0
 
   var currApple: Point = appleGenerator()
-  //var appleExists : Boolean = true
 
   var (x, y) = initializeGame()
   snakeHeadPos = Point(x, y)
   snakeHeadDir = East()
+  var directionChanged : Boolean = false
 
   def step(): Unit = {
+    if (tailHit){
+      return
+    } //stops the game moving
 
-    if (tailHit) return //stops the game moving
-
+    directionChanged = false
     val bodyWithHeadSnake : List[Point] = snakeHeadPos :: snakeBodyPos
 
     var listLength : Int = snakeBodyPos.length - 1
@@ -111,85 +102,23 @@ class GameLogic(val random: RandomGenerator,
       snakeHeadPos = Point(x, y)
     }
 
-    // Updates the snake body positions
-    /*for (i <- (1 to listLength).reverse) {
-      var newSnakeBody : List[Point] = List[Point]()
-      snakeBodyPos(i) = snakeBodyPos(i - 1)*
-    }*/
-
     //Updates the snake body positions
     if(counter == 0)
     {
       val tempSnakeCopy : List[Point] = bodyWithHeadSnake.init
       val newSnakeBodyPos : List[Point] = tempSnakeCopy
-      //val newSnakeBodyPos : List[Point] = snakeHeadPos :: tempSnakeCopy
       snakeBodyPos = newSnakeBodyPos
     }
     else
     {
       val newSnakeBodyPos: List[Point] = bodyWithHeadSnake
-      //val newSnakeBodyPos : List[Point] = snakeHeadPos :: tempSnakeCopy
       snakeBodyPos = newSnakeBodyPos
       counter -= 1
     }
 
-   /* if (!(appleExists)) {
-      currApple = appleGenerator()
-      appleExists = true
-    }*/
-
-
-    // Updates the first element of the snake body to follow the head
-   // snakeBodyPos(0) = snakeHeadPos
-
     if(snakeHeadPos == currApple) //TODO make it a function
     {
-      //appleExists = false
-
       currApple = appleGenerator()
-
-      val tailX : Int = snakeBodyPos(listLength).x
-      val tailY : Int = snakeBodyPos(listLength).y
-
-      /*if(directionFlag.up)
-      {
-        /*snakeBodyPos.append(Point(tailX, tailY + 1))
-        snakeBodyPos.append(Point(tailX, tailY + 2))
-        snakeBodyPos.append(Point(tailX, tailY + 3))*/
-        //TODO maybe a function to add stuff to a list?
-        val newElements : List[Point] = List[Point] (Point(tailX, tailY + 1), Point(tailX, tailY + 2), Point(tailX, tailY + 3))
-        val newSnakeBodyPos : List[Point] = snakeBodyPos ::: newElements
-        snakeBodyPos = newSnakeBodyPos
-      }
-      else if(directionFlag.down)
-      {
-        /*snakeBodyPos.append(Point(tailX, tailY - 1))
-        snakeBodyPos.append(Point(tailX, tailY - 2))
-        snakeBodyPos.append(Point(tailX, tailY - 3))*/
-
-        val newElements: List[Point] = List[Point](Point(tailX, tailY - 1), Point(tailX, tailY - 2), Point(tailX, tailY - 3))
-        val newSnakeBodyPos: List[Point] = snakeBodyPos ::: newElements
-        snakeBodyPos = newSnakeBodyPos
-      }
-      else if(directionFlag.right)
-      {
-        /*snakeBodyPos.append(Point(tailX - 1, tailY))
-        snakeBodyPos.append(Point(tailX - 2, tailY))
-        snakeBodyPos.append(Point(tailX - 3, tailY))*/
-
-        val newElements: List[Point] = List[Point](Point(tailX - 1, tailY), Point(tailX - 2, tailY), Point(tailX - 3, tailY))
-        val newSnakeBodyPos: List[Point] = snakeBodyPos ::: newElements
-        snakeBodyPos = newSnakeBodyPos
-      }
-      else
-      {
-        /*snakeBodyPos.append(Point(tailX + 1, tailY))
-        snakeBodyPos.append(Point(tailX + 2, tailY))
-        snakeBodyPos.append(Point(tailX + 3, tailY)) */
-        val newElements: List[Point] = List[Point](Point(tailX + 1, tailY), Point(tailX + 2, tailY), Point(tailX + 3, tailY))
-        val newSnakeBodyPos: List[Point] = snakeBodyPos ::: newElements
-        snakeBodyPos = newSnakeBodyPos
-      }*/
       counter = 3
       listLength += 3
     }
@@ -232,32 +161,32 @@ class GameLogic(val random: RandomGenerator,
   }
 
   def changeDir(d: Direction): Unit = {
-    //test = true
     snakeHeadDir = d
 
-    if(tailHit) return
+  if(tailHit) {
+    return
+  } //doesn't allow for direction change after game over
 
-    d match
-    {
-      case North() =>
-        directionFlag.resetDirFlags()
-        directionFlag.up = true
+  d match
+  {
+    case North() =>
+      directionFlag.resetDirFlags()
+      directionFlag.up = true
 
-      case South() =>
-        directionFlag.resetDirFlags()
-        directionFlag.down = true
+    case South() =>
+      directionFlag.resetDirFlags()
+      directionFlag.down = true
 
-      case West() =>
-        directionFlag.resetDirFlags()
-        directionFlag.left = true
+    case West() =>
+      directionFlag.resetDirFlags()
+      directionFlag.left = true
 
-      case East() =>
-        directionFlag.resetDirFlags()
-        directionFlag.right = true
-    }
+    case East() =>
+      directionFlag.resetDirFlags()
+      directionFlag.right = true
   }
+}
 
-  // TODO implement me
   def getCellType(p : Point): CellType = {
 
     if(p == snakeHeadPos)
@@ -279,9 +208,6 @@ class GameLogic(val random: RandomGenerator,
       return Empty()
   }
 
-
-
-  // TODO implement me
   def setReverse(r: Boolean): Unit = ()
 
 }
@@ -289,7 +215,7 @@ class GameLogic(val random: RandomGenerator,
 /** GameLogic companion object */
 object GameLogic {
 
-  val FramesPerSecond: Int = 5 // change this to increase/decrease speed of game
+  val FramesPerSecond: Int = 1 // change this to increase/decrease speed of game
 
   val DrawSizeFactor = 1.0 // increase this to make the game bigger (for high-res screens)
   // or decrease to make game smaller
